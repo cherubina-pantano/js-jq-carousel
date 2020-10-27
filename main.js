569c22611524e158603c80ddeb272379d2bb2a23
cherubina-pantano <cherubina.pantano@gmail.com>
2020-10-27 16:34:50 +0100

    Direction: prev

--- main.js
+++ main.js
@@ -5,15 +5,12 @@ $(document).ready( function () {
    // Al click del maouse passo all'img successiva
   $('.next').click( function () {
     nextPrevImg('next');
-
   });
 
   // Al click del maouse torno all'img precedente
   $('.prev').click( function () {
-
+    nextPrevImg('prev');
   });
-
-
 
 });
 
@@ -28,6 +25,8 @@ function nextPrevImg(direction) {
   var circleActive = $('.nav i.active');
   var imgFirst = $('.images img.first');
   var circleFirst = $('.nav i.first');
+  var imgLast = $('.images img.last');
+  var circleLast = $('.nav i.last');
 
   // Reset per togliere la classe 'active' ai precedenti
   imgActive.removeClass('active');
@@ -43,5 +42,18 @@ function nextPrevImg(direction) {
       imgActive.next('img').addClass('active');
       circleActive.next('i').addClass('active');
     }
+  }
+
+  // Direzione <-- PREV
+  else if (direction === 'prev') {
+    if(imgActive.hasClass('first') ) {
+      imgLast.addClass('active');
+      circleLast.addClass('active');
+    }
+    else {
+      imgActive.prev('img').addClass('active');
+      circleActive.prev('i').addClass('active');
+    }
+
   }
 }
